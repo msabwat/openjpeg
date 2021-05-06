f4cb033a340b55dbc576453c4b6a967fec5cbbda
Mehdi Sabwat <mehdisabwat@gmail.com>
2021-05-07 01:50:37 +0200

    fix heap buffer overflow #1347

--- src/bin/common/color.c
+++ src/bin/common/color.c
@@ -368,12 +368,15 @@ static void sycc420_to_rgb(opj_image_t *img)
 
             sycc_to_rgb(offset, upb, *y, *cb, *cr, r, g, b);
 
-            ++y;
+            if (*y != img->comps[0].data[loopmaxh])
+                ++y;
             ++r;
             ++g;
             ++b;
-            ++cb;
-            ++cr;
+            if (*cb != img->comps[1].data[loopmaxh])
+                ++cb;
+            if (*cr != img->comps[2].data[loopmaxh])
+                ++cr;
         }
         if (j < maxw) {
             sycc_to_rgb(offset, upb, *y, *cb, *cr, r, g, b);
